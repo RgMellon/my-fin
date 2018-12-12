aa97b8932e20ee21adc188eadd084f9ce937caab
Renan <renan_gian@hotmail.com>
2018-12-11 22:38:34 -0200

    yesye

--- src/pages/datas/dia.vue
+++ src/pages/datas/dia.vue
@@ -1,15 +1,18 @@
 <template>
-    <page-wrapper>
-      <template slot="conteudo">
-          <section class="grafico row justify-center">
-            <knob :maximo="20" :minimo="0" :atual="1"> </knob>
-          </section>
-          <slider gasto="20,00" sobra="10,00" total="50,00"> </slider>
-          <div class="btn-adiciona">
-            <btn-despesas></btn-despesas>
-          </div>
-      </template>
-    </page-wrapper>
+  <page-wrapper>
+    <template slot="conteudo">
+      <section class="grafico row justify-center">
+        <knob :maximo="30" :minimo="0" :atual="5"> </knob>
+      </section>
+
+      <div class="balanco">
+        <slider gasto="200,00" sobra="100,00" total="600,00"> </slider>
+        <div class="btn-adiciona">
+          <btn-despesas></btn-despesas>
+        </div>
+    </div>
+    </template>
+  </page-wrapper>
 </template>
 
 <script>
@@ -21,16 +24,25 @@ import BtnDespesas from '../../components/BtnDespesas';
 
 export default {
   components: {
-    'page-wrapper': wrapper,
+    'page-wrapper' : wrapper,
     'slider': Slider,
     'knob': Knob,
     'btn-despesas' : BtnDespesas
   },
-  name: 'PageDia'
+  data () {
+    return {
+      model: 300,
+      min: 0,
+      max: 400,
+    }
+  },
+
+
 }
 </script>
 
 <style>
+
 .btn-adiciona {
   position: fixed;
   bottom: 0;
@@ -38,4 +50,41 @@ export default {
   width: 100%;
   height: 50px;
 }
+.info {
+  color: gray;
+}
+
+.titulo {
+  color: #9c27b0;
+  margin-top: 1.3rem;
+}
+.first-c {
+  margin-top: 2.4rem;
+  border-radius: 10px;
+  background: white;
+  color: black;
+  height: 186px;
+}
+
+.info {
+  font-size: 1rem;
+}
+.s-c {
+  border-radius: 10px;
+  background: #9c27b0;
+}
+.titulo  {
+  color: #9c27b0;
+  font-size: 1srem;
+}
+
+.grafico {
+  margin-top: 2rem;
+}
+
+.meio {
+  background: #9c27b0;
+}
+
+
 </style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,16 +4,16 @@ const routes = [
     path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: '/', component: () => import('pages/datas/dia.vue') },
-      { path: '/mes', component: () => import('pages/datas/mes.vue') },
-      { path: '/semana', component: () => import('pages/datas/semana.vue') }
+      { path: '', component: () => import('pages/datas/dia.vue') },
+      { path: 'mes', component: () => import('pages/datas/mes.vue') },
+      { path: 'semana', component: () => import('pages/datas/semana.vue') }
     ]
   },
   {
     path: '/adiciona',
     component: () => import('layouts/LayoutTwo.vue'),
     children: [
-      { path: '/adiciona/despesas', component: () => import('pages/adicionar-despesas.vue') }
+      { path: 'despesas', component: () => import('pages/adicionar-despesas.vue') }
 
     ]
   }
